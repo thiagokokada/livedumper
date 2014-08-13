8f167fbefc8571fa13118286f2b4f7d63cab1198
Thiago Kenji Okada <thiago.mast3r@gmail.com>
2014-08-13 14:01:05 -0300

    Bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name="livedumper",
-    version="0.1.3",
+    version="0.1.4",
     author="Thiago Kenji Okada",
     author_email="thiago.mast3r@gmail.com",
     description=("Livestreamer stream dumper"),
