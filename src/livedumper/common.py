9f0a6783e379db04e3bfa6db895bfb024173641b
Thiago Kenji Okada <thiago.mast3r@gmail.com>
2014-08-19 22:21:13 -0300

    Added all __future__ statements for all modules
    This should bring some peace of mind for me in the future, even if it's
    unecessary for now.

--- src/livedumper/common.py
+++ src/livedumper/common.py
@@ -1,6 +1,7 @@
 "Common functions that may be used everywhere"
 
-from __future__ import print_function, unicode_literals
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
 
 import os
 import sys
--- src/livedumper/dumper.py
+++ src/livedumper/dumper.py
@@ -1,6 +1,7 @@
 "Livestreamer main class"
 
-from __future__ import division, print_function, unicode_literals
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
 
 import os
 import re
